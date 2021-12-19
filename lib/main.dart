2b4c0f994f881053c9528bb6054228496e931b4c
LAPTOP-6HJQS4BE\Seishiro <umaumaumainoue@yahoo.co.jp>
2021-12-19 16:43:24 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:photo_gallery/home.dart';
+import 'package:photo_gallery/login.dart';
+
+import 'package:photo_gallery/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Login(),
+      routes: {
+        '/home' : (context) => Home(),
+      },
+    );
+  }
+}
